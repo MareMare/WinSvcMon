ce4f7097b11f62df787d69c91b40ed221c0b1994
MareMare <MareMare@users.noreply.github.com>
2025-06-19 16:20:14 +0900

    イベントハンドラの登録をデザイナコードから通常のコードに移動します。 (#3)

--- src/WinSvcMon.SdkStyle.App/MainForm.Designer.cs
+++ src/WinSvcMon.SdkStyle.App/MainForm.Designer.cs
@@ -15,77 +15,80 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
-        var resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-        this.servicesGridView = new DataGridView();
-        this.ColumnOfDisplayName = new DataGridViewTextBoxColumn();
-        this.ColumnOfServiceName = new DataGridViewTextBoxColumn();
-        this.ColumnOfServiceState = new DataGridViewTextBoxColumn();
-        this.ColumnToChangeState = new DataGridViewButtonColumn();
-        ((System.ComponentModel.ISupportInitialize)this.servicesGridView).BeginInit();
-        this.SuspendLayout();
-        // 
-        // servicesGridView
-        // 
-        this.servicesGridView.AllowUserToAddRows = false;
-        this.servicesGridView.AllowUserToDeleteRows = false;
-        this.servicesGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        this.servicesGridView.Columns.AddRange(new DataGridViewColumn[] { this.ColumnOfDisplayName, this.ColumnOfServiceName, this.ColumnOfServiceState, this.ColumnToChangeState });
-        this.servicesGridView.Dock = DockStyle.Fill;
-        this.servicesGridView.Location = new Point(10, 10);
-        this.servicesGridView.MultiSelect = false;
-        this.servicesGridView.Name = "servicesGridView";
-        this.servicesGridView.ReadOnly = true;
-        this.servicesGridView.RowHeadersVisible = false;
-        this.servicesGridView.RowTemplate.DefaultCellStyle.Font = new Font("BIZ UDゴシック", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
-        this.servicesGridView.RowTemplate.Height = 30;
-        this.servicesGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-        this.servicesGridView.Size = new Size(604, 421);
-        this.servicesGridView.TabIndex = 2;
-        this.servicesGridView.CellContentClick += this.OnServicesGridViewCellContentClick;
-        // 
-        // ColumnOfDisplayName
-        // 
-        this.ColumnOfDisplayName.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-        this.ColumnOfDisplayName.HeaderText = "サービス表示名";
-        this.ColumnOfDisplayName.Name = "ColumnOfDisplayName";
-        this.ColumnOfDisplayName.ReadOnly = true;
-        // 
-        // ColumnOfServiceName
-        // 
-        this.ColumnOfServiceName.FillWeight = 120F;
-        this.ColumnOfServiceName.HeaderText = "サービス名";
-        this.ColumnOfServiceName.Name = "ColumnOfServiceName";
-        this.ColumnOfServiceName.ReadOnly = true;
-        this.ColumnOfServiceName.Width = 160;
-        // 
-        // ColumnOfServiceState
-        // 
-        this.ColumnOfServiceState.HeaderText = "状態";
-        this.ColumnOfServiceState.Name = "ColumnOfServiceState";
-        this.ColumnOfServiceState.ReadOnly = true;
-        // 
-        // ColumnToChangeState
-        // 
-        this.ColumnToChangeState.HeaderText = "操作";
-        this.ColumnToChangeState.Name = "ColumnToChangeState";
-        this.ColumnToChangeState.ReadOnly = true;
-        this.ColumnToChangeState.Text = "切替";
-        this.ColumnToChangeState.UseColumnTextForButtonValue = true;
-        // 
-        // MainForm
-        // 
-        this.AutoScaleMode = AutoScaleMode.None;
-        this.ClientSize = new Size(624, 441);
-        this.Controls.Add(this.servicesGridView);
-        this.Font = new Font("BIZ UDゴシック", 12F);
-        this.Icon = (Icon)resources.GetObject("$this.Icon");
-        this.Name = "MainForm";
-        this.Padding = new Padding(10);
-        this.Text = "Windows Service Monitor";
-        this.FormClosing += this.MainForm_FormClosing;
-        this.Load += this.MainForm_Load;
-        ((System.ComponentModel.ISupportInitialize)this.servicesGridView).EndInit();
-        this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.servicesGridView = new System.Windows.Forms.DataGridView();
+            this.ColumnOfDisplayName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnOfServiceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnOfServiceState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnToChangeState = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.servicesGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // servicesGridView
+            // 
+            this.servicesGridView.AllowUserToAddRows = false;
+            this.servicesGridView.AllowUserToDeleteRows = false;
+            this.servicesGridView.AllowUserToResizeRows = false;
+            this.servicesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.servicesGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnOfDisplayName,
+            this.ColumnOfServiceName,
+            this.ColumnOfServiceState,
+            this.ColumnToChangeState});
+            this.servicesGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.servicesGridView.Location = new System.Drawing.Point(10, 10);
+            this.servicesGridView.MultiSelect = false;
+            this.servicesGridView.Name = "servicesGridView";
+            this.servicesGridView.ReadOnly = true;
+            this.servicesGridView.RowHeadersVisible = false;
+            this.servicesGridView.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("BIZ UDゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.servicesGridView.RowTemplate.Height = 30;
+            this.servicesGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.servicesGridView.Size = new System.Drawing.Size(604, 421);
+            this.servicesGridView.TabIndex = 2;
+            // 
+            // ColumnOfDisplayName
+            // 
+            this.ColumnOfDisplayName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColumnOfDisplayName.HeaderText = "サービス表示名";
+            this.ColumnOfDisplayName.Name = "ColumnOfDisplayName";
+            this.ColumnOfDisplayName.ReadOnly = true;
+            // 
+            // ColumnOfServiceName
+            // 
+            this.ColumnOfServiceName.FillWeight = 120F;
+            this.ColumnOfServiceName.HeaderText = "サービス名";
+            this.ColumnOfServiceName.Name = "ColumnOfServiceName";
+            this.ColumnOfServiceName.ReadOnly = true;
+            this.ColumnOfServiceName.Width = 160;
+            // 
+            // ColumnOfServiceState
+            // 
+            this.ColumnOfServiceState.HeaderText = "状態";
+            this.ColumnOfServiceState.Name = "ColumnOfServiceState";
+            this.ColumnOfServiceState.ReadOnly = true;
+            // 
+            // ColumnToChangeState
+            // 
+            this.ColumnToChangeState.HeaderText = "操作";
+            this.ColumnToChangeState.Name = "ColumnToChangeState";
+            this.ColumnToChangeState.ReadOnly = true;
+            this.ColumnToChangeState.Text = "切替";
+            this.ColumnToChangeState.UseColumnTextForButtonValue = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.servicesGridView);
+            this.Font = new System.Drawing.Font("BIZ UDゴシック", 12F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Windows Service Monitor";
+            ((System.ComponentModel.ISupportInitialize)(this.servicesGridView)).EndInit();
+            this.ResumeLayout(false);
+
     }
 
     #endregion
